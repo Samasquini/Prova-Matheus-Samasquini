18bc89b64f0f19179c586e744709047ee67dcb84
Samasquini <msamasquini321@gmail.com>
2023-06-13 14:50:34 -0300

    quarta versao

--- Prova2Bim/Prova.Designer.cs
+++ Prova2Bim/Prova.Designer.cs
@@ -28,37 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox9 = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.textBox10 = new System.Windows.Forms.TextBox();
             this.label10 = new System.Windows.Forms.Label();
-            this.textBox8 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox5 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox6 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox7 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox8 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox9 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox10 = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(13, 44);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
@@ -78,13 +71,6 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Questao 2:";
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(13, 88);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 2;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
@@ -93,13 +79,6 @@
             this.label3.Size = new System.Drawing.Size(71, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "Questao 3:";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(12, 132);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 4;
             // 
             // label4
             // 
@@ -110,13 +89,6 @@
             this.label4.TabIndex = 7;
             this.label4.Text = "Questao 5:";
             // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(12, 220);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 22);
-            this.textBox5.TabIndex = 6;
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
@@ -125,13 +97,6 @@
             this.label5.Size = new System.Drawing.Size(71, 16);
             this.label5.TabIndex = 9;
             this.label5.Text = "Questao 6:";
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(177, 44);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 22);
-            this.textBox6.TabIndex = 8;
             // 
             // label6
             // 
@@ -142,13 +107,6 @@
             this.label6.TabIndex = 11;
             this.label6.Text = "Questao 7:";
             // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(177, 88);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 22);
-            this.textBox7.TabIndex = 10;
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
@@ -157,13 +115,6 @@
             this.label7.Size = new System.Drawing.Size(71, 16);
             this.label7.TabIndex = 13;
             this.label7.Text = "Questao 9:";
-            // 
-            // textBox9
-            // 
-            this.textBox9.Location = new System.Drawing.Point(177, 176);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(100, 22);
-            this.textBox9.TabIndex = 12;
             // 
             // label8
             // 
@@ -174,13 +125,6 @@
             this.label8.TabIndex = 15;
             this.label8.Text = "Questao 10:";
             // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(12, 176);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
-            this.textBox4.TabIndex = 14;
-            // 
             // label9
             // 
             this.label9.AutoSize = true;
@@ -190,13 +134,6 @@
             this.label9.TabIndex = 17;
             this.label9.Text = "Questao 4:";
             // 
-            // textBox10
-            // 
-            this.textBox10.Location = new System.Drawing.Point(177, 220);
-            this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(100, 22);
-            this.textBox10.TabIndex = 16;
-            // 
             // label10
             // 
             this.label10.AutoSize = true;
@@ -205,13 +142,6 @@
             this.label10.Size = new System.Drawing.Size(71, 16);
             this.label10.TabIndex = 19;
             this.label10.Text = "Questao 8:";
-            // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(177, 132);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 22);
-            this.textBox8.TabIndex = 18;
             // 
             // button1
             // 
@@ -243,34 +173,114 @@
             this.label12.TabIndex = 22;
             this.label12.Text = "-------";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 42);
+            this.textBox1.Mask = "a";
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 23;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 88);
+            this.textBox2.Mask = "a";
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 24;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(16, 132);
+            this.textBox3.Mask = "a";
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 25;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(16, 176);
+            this.textBox4.Mask = "a";
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 26;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(16, 220);
+            this.textBox5.Mask = "a";
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 27;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(177, 44);
+            this.textBox6.Mask = "a";
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 28;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(177, 88);
+            this.textBox7.Mask = "a";
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 29;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(177, 132);
+            this.textBox8.Mask = "a";
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 30;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(177, 176);
+            this.textBox9.Mask = "a";
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 31;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(177, 220);
+            this.textBox10.Mask = "a";
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 22);
+            this.textBox10.TabIndex = 32;
+            // 
             // Prova
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label10);
-            this.Controls.Add(this.textBox8);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.textBox10);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox9);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox7);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
             this.Name = "Prova";
             this.Text = "Prova";
             this.Load += new System.EventHandler(this.Prova_Load);
@@ -280,29 +290,28 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox10;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.MaskedTextBox textBox1;
+        private System.Windows.Forms.MaskedTextBox textBox2;
+        private System.Windows.Forms.MaskedTextBox textBox3;
+        private System.Windows.Forms.MaskedTextBox textBox4;
+        private System.Windows.Forms.MaskedTextBox textBox5;
+        private System.Windows.Forms.MaskedTextBox textBox6;
+        private System.Windows.Forms.MaskedTextBox textBox7;
+        private System.Windows.Forms.MaskedTextBox textBox8;
+        private System.Windows.Forms.MaskedTextBox textBox9;
+        private System.Windows.Forms.MaskedTextBox textBox10;
     }
 }
